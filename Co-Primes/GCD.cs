83cd5625e8fa4c4ca506d2e17735d6dcee27c3ff
intern.tanvi@astrumlabs.co <intern.tanvi@astrumlabs.co>
2019-08-02 18:36:43 +0530

    Add project files.

---
+++ Co-Primes/GCD.cs
@@ -0,0 +1,20 @@
+﻿namespace Co_Primes
+{
+    public class GCDCalculator
+    {
+        public static int FindGCD(int x, int y)
+        {
+            int smallestNumber = (x < y) ? x : y;
+            int gcd = 1;
+            for (int i = smallestNumber; i >= 2; i--)
+            {
+                if (x % i == 0 && y % i == 0)
+                {
+                    gcd = i;
+                }
+            }
+
+            return gcd;
+        }
+    }
+}
---
+++ Co-Primes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Co_Primes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start, end;
+
+            Console.WriteLine("Enter start of range");
+            start = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter end of range");
+            end = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = start; i < end; i++)
+            {
+                for (int j = i + 1; j <= end; j++)
+                {
+                    if (GCDCalculator.FindGCD(i, j) == 1)
+                    {
+                        Console.WriteLine($"{i} and {j} are co-primes");
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
